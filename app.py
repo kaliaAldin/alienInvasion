163ab31636a0a248164833c07e96948ddaa34900
ahmed <ahmedisamaldin@gmail.com>
2023-11-13 14:03:19 +0100

    make setting file

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 import pygame
 import sys
-
+from setting import Settings
+ai_setting = Settings()
 def run_game():
+    #initialising pygame
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    #setting a display
+    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    bg_color = (255,0,0)
+
 
     while True :
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.fill(ai_setting.bg_color)
         pygame.display.flip()
-        screen.fill(bg_color)
+
 
 
 run_game()
