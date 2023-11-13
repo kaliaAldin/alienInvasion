f20bc817f1029fddcd555d9e413576b96e9dc384
ahmed <ahmedisamaldin@gmail.com>
2023-11-13 13:42:24 +0100

    frist commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import pygame
+import sys
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Alien Invasion")
+    bg_color = (255,0,0)
+
+    while True :
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        pygame.display.flip()
+        screen.fill(bg_color)
+
+
+run_game()
